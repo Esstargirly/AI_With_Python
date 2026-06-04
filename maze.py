47f9097098f0a2dfe7a5bc1575b408bb4df63e11
Esstargirly <estherappolos51@gmail.com>
2026-06-04 13:18:34 +0100

    Fix node parent initialization, correct state reference in solution tracking, and implement maze output image generation

--- maze.py
+++ maze.py
@@ -123,7 +123,7 @@ class Maze():
         self.num_explored = 0
 
         #Initialize frontier to just the starting position
-        start = Node(state=self.start, parent=Node, action=None)
+        start = Node(state=self.start, parent=None, action=None)
         frontier = StackFrontier()
         frontier.add(start)
 
@@ -149,7 +149,7 @@ class Maze():
                 #Follow parent nodes to find solution 
                 while node.parent is not None:
                     actions.append(node.action)
-                    cells.append(node.start)
+                    cells.append(node.state)
                     node = node.parent
                 actions.reverse()
                 cells.reverse()
@@ -161,9 +161,71 @@ class Maze():
 
             #mark neighbours to frontier
             for action, state in self.neighbours(node.state):
-                if not frontier.contians_state(state) and state not in self.explored:
+                if not frontier.contains_state(state) and state not in self.explored:
                     child = Node(state=state, parent=node, action=action)
                     frontier.add(child)
+
+    def output_image(self, filename, show_solution= True, show_explored=False):
+        from PIL import Image, ImageDraw
+        cell_size = 50
+        cell_border = 2
+
+        #create a blank canvas
+        img = Image.new(
+            "RGBA",
+            (self.width * cell_size, self.height * cell_size),
+             "black"
+        )
+        draw = ImageDraw.Draw(img)
+
+        solution = self.solution[1] if self.solution is not None else None
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+
+                #Walls
+                if col:
+                    fill = (40, 40, 40)
+
+                #start
+                elif (i, j) == self.start:
+                    fill = (255, 0, 0)
+                
+                #Goal
+                elif (i, j) == self.goal:
+                    fill = (0, 171, 28)
+
+                #solution 
+                elif solution is not None and show_solution and (i, j) in solution:
+                    fill = (220, 235, 113)
+
+                #explored 
+                elif solution is not None and show_solution and (i, j) in self.explored:
+                    fill = (212, 97, 85)
+
+                #empty
+                else:
+                    fill = (237, 240, 252)
+
+                #Draw cell
+                draw.rectangle(
+                    [(j * cell_size + cell_border, i * cell_size + cell_border),
+                    ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)],
+                    fill= fill
+                )
+        img.save(filename)
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: Python maze.py maze.txt")
+
+m = Maze(sys.argv[1])
+print("Maze")
+m.print()
+print("Solving...")
+m.solve()
+print("States Explored:", m.num_explored)
+print("Solution:")
+m.print()
+m.output_image("maze.png", show_explored=True)
                 
 
 
