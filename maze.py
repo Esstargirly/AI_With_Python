4d72bead937d8b24dd73ecc8df34dff16747f970
Esstargirly <estherappolos51@gmail.com>
2026-06-04 09:37:48 +0100

    Add Maze class to handle maze file reading and validation

--- maze.py
+++ maze.py
@@ -46,4 +46,69 @@ class Maze():
             contents = f.read()
 
         #Validate start and goal 
-        if contents.count("A") ! = 1
+        if contents.count("A") != 1:
+            raise Exception("Maze must have exactly one start point")
+        if contents.count("B") != 1:
+            raise Exception ("Maze must have exactly one goal")
+        
+        #determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        #Keep track of walls
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents [i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents [i][j] == "B":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents [i] [j] == "":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+        self.solution = None
+
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print(None)
+        for i, row in enumerate (self.walls):
+            for j, col in enumerate(row):
+                if col:
+                    print("", end="")
+                elif (i, j) == self.start:
+                    print("A", end="")
+                elif (i, j) == self.goal:
+                    print("B", end="")
+                elif solution is not None and (i, j) in solution:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
+        print()
+
+    def neighbours (self, state):
+        row, col = state
+
+        #all possible actions
+        candidates = [
+            ("up", (row - 1, col)),
+            ("down", (row + 1, col)),
+            ("left", (row, col -1)),
+            ("right", (row, col +1))
+        ]
+
+    
+                
+
+
+
+
